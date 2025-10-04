0a4100c17486dadf1720f6c609db0197f37d19f1
Fajar Rizki Ramadhan <2110511029@mahasiswa.upnvj.ac.id>
2025-10-04 17:33:55 +0700

    my second commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,41 +1,13 @@
-// import "./globals.css";
-// import localFont from "next/font/local";
-
-// const atahaya = localFont({
-//   src:"../../public/fonts/Atahaya-Regular.ttf",
-//   variable: "--font-atahaya",
-// });
-
-// export const metadata = {
-//   title: "Akmal & Marisa Wedding Invitation",
-//   description: "Undangan pernikahan digital",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="id">
-//       <body className={`${atahaya.variable} font-sans`}>
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
 "use client";
 
 import "./globals.css";
 import { AppProvider } from "@/context/AppContext";
-import { ayuthaya, gardena, zapfino, philosopher } from "@/styles/fonts";
+import "../styles/fonts.css";
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en"
-         className={`
-        ${ayuthaya.variable} 
-        ${gardena.variable} 
-        ${zapfino.variable} 
-        ${philosopher.variable}
-      `}
       >
       <body>
         <AppProvider>
